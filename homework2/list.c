295c7bc8ec43d73c51460394c15d1e0daa2c509c
YingLyu <lyuying@bupt.edu.cn>
2019-01-23 15:20:04 +0800

    previous homework2/list.c version 2

--- homework2/list.c
+++ homework2/list.c
@@ -1,17 +1,14 @@
 #include <unistd.h>
 #include <getopt.h>
 #include <stdio.h>
-#include <dirent.h>
-#include <errno.h>
+
 
 /* list main function */
 int main(int argc, char **argv)
 {
-	DIR *dir;
-	struct dirent *entry; 
-	int c;
-	int low = 0, high = 1000000, modify = 100;
-	int errflg = 0, helpflg = 0, lflg = 0, hflg = 0, mflg = 0, rflg = 0, aflg = 0;
+  int c;
+  int low = 0, high = 1000000, modify = 100;
+  int errflg = 0, helpflg = 0, lflg = 0, hflg = 0, mflg = 0, rflg = 0, aflg = 0;
     	
   struct option longopts[] =
   {
@@ -30,55 +27,46 @@ int main(int argc, char **argv)
     {
     	case 'l':
     		low = atoi(optarg);
-        	printf("low = %d B\n",low);
-        	lflg = 1;
+        	printf(low = %d B\n",low);
+        	lflag = 1;
         break;
       	case 'h':
     		high = atoi(optarg);
         	printf("high = %d B\n",high);
-        	hflg = 1;
+        	hflag = 1;
         break;
       	case 'm':
     		modify = atoi(optarg);
         	printf("modify within %d day(s)\n",modify);
-        	mflg = 1;
+        	mflag = 1;
         break;
       	case 'r':
 			printf("recursion");
-			rflg = 1;
+			rflag = 1;
         break;
       	case 'a':
         	printf("all");
-        	aflg = 1;
+        	aflag = 1;
     	break;
 
       	case '?':
         	errflg++;
         break;
       	default:
-        	
-        break;
+        	break;
     }
   }
-	dir = opendir(argv[optind]); 
-	if (dir == NULL) {
-		printf("Open directory \"%s\": %s (ERROR %d)\n", 
-			argv[optind], strerror(errno), errno);
-	return 1; 
-	}
-	while ((entry = readdir(dir)) != NULL)
-		printf("%4d %s \n", entry->d_ino, entry->d_name);
-	closedir(dir);
+
   if(errflg) 
   {
-     fprintf(stderr,"Usage:    wsiod [OPTION]\n\n",
+     fprintf(stderr,"Usage:    wsiod [OPTION]\n\n%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s",
          "WSIO server based on web service.\n",
     "Mandatory arguments to long options are mandatory for short options too.\n",
-    "\t-r, --recursion   show the subfile\n ",
-    "\t-a, --all         show the file start with dot\n",
-    "\t-l, --low        minimum of size, default is 0\n",
-    "\t-h, --high        maximum of size, default is\n",
-    "\t-m, --modify     modify date\n",
+    "\t-r, --recursion           hostname in soap_bind, default is host which the service runs\n ",
+    "\t-a, --all         port which the sercer runs on, default is 8080\n",
+    "\t-l, --low        request backlog, default is 100\n",
+    "\t-h, --high        server type, default is COMMON\n",
+    "\t-m, --modify     attempt to keep socket connections alive\n",
     "\t    --help        print this help\n\n",
 
     "Report bugs to <lyuying@bupt.edu.cn>.\n"
