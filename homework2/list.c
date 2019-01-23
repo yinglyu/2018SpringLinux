f18ee969a053826f259348c2887cf6254cc38076
YingLyu <lyuying@bupt.edu.cn>
2019-01-23 15:25:58 +0800

    previous homework2/list.c version 6

--- homework2/list.c
+++ homework2/list.c
@@ -1,16 +1,71 @@
 #include <unistd.h>
 #include <getopt.h>
 #include <stdio.h>
+#include <dirent.h>
+#include <errno.h>
+#include <sys/stat.h> 
+#include <time.h>
+#define DAY_SEC 86400
+#define BUFLEN 255 
+time_t now;
+long int time_limit;
+int errflg = 0, helpflg = 0, lflg = 0, hflg = 0, mflg = 0, rflg = 0, aflg = 0;
+int low = 0, high = 1000000, modify = 100;
 
+int recursion_dir(char * path){
+	struct dirent *entry;
+	struct stat st;
+	DIR *dir;
+	int ret = 0;
+	char  new_path[BUFLEN],format_time[BUFLEN];
+	dir = opendir(path);
+	if (dir == NULL) {
+            printf("Open directory %s: %s (ERROR %d)\n",path, strerror(errno), errno);
+        return 1;//errno 
+        }
+    while ((entry = readdir(dir)) != NULL){
+ 		sprintf(new_path,"%s/%s",path,entry->d_name);
+        ret = stat(new_path,&st);
+		/*printf("%s\n",new_path);test*/
+        if (ret == -1){
+			printf("error: %m\n");
+		}
+		if(!S_ISDIR(st.st_mode)&
+				((mflg&(st.st_mtime>time_limit))|(!mflg))&
+                (lflg&(st.st_size>low)|(!lflg))&
+                (hflg&(st.st_size<high)|(!hflg))
+                 )
+        {
+			strftime(format_time, BUFLEN, "%F", localtime(&(st.st_mtime)));
+			printf("%s\t\t",format_time);
+			printf("%ld\t\t",st.st_size);
+            printf("%s\n",  entry->d_name);
+			//printf("%ld\t\t",st.st_mtime);
 
-/* list main function */
+        }
+	//	printf("d_name%s\n",entry->d_name);
+        if(S_ISDIR(st.st_mode)&(entry->d_name[0]!= '.')){
+        	
+			printf("%s:\n",new_path);
+             recursion_dir(new_path);
+			printf("%s end\n",new_path);          
+        }
+    }
+	closedir(dir);
+
+}
+
 int main(int argc, char **argv)
 {
-  int c;
-  int low = 0, high = 1000000, modify = 100;
-  int errflg = 0, helpflg = 0, lflg = 0, hflg = 0, mflg = 0, rflg = 0, aflg = 0;
-    	
-  struct option longopts[] =
+	char *path,format_time[BUFLEN],new_path[BUFLEN];//单纯的 节点无法访问../目录
+	DIR *dir;
+	struct dirent *entry; //dirent.h
+ 	struct stat st;//<sys/stat.h>
+	int ret;
+	int c;
+	
+ 	
+  struct option longopts[] =//getopt.h
   {
     {"low", 1, 0, 'l'},
     {"high", 1, 0, 'h'},
@@ -27,25 +82,25 @@ int main(int argc, char **argv)
     {
     	case 'l':
     		low = atoi(optarg);
-        	printf("low = %d B\n",low);
+        	printf("low = %d B\t",low);
         	lflg = 1;
         break;
       	case 'h':
     		high = atoi(optarg);
-        	printf("high = %d B\n",high);
+        	printf("high = %d B\t",high);
         	hflg = 1;
         break;
       	case 'm':
     		modify = atoi(optarg);
-        	printf("modify within %d day(s)\n",modify);
+        	printf("modify within %d day(s)\t",modify);
         	mflg = 1;
         break;
       	case 'r':
-			printf("recursion\n");
+			printf("recursion\t");
 			rflg = 1;
         break;
       	case 'a':
-        	printf("all\n");
+        	printf("all\t");
         	aflg = 1;
     	break;
 
@@ -57,11 +112,87 @@ int main(int argc, char **argv)
         break;
     }
   }
+    if (mflg){
+       time(&now);
+       strftime(format_time, BUFLEN, "%F", localtime(&now));
+       printf("today date : %s\t\t",format_time);
+       time_limit = now - modify*DAY_SEC;
+    }
+	printf("optind:%d\t",optind);/*第一个非选项参数出现位置*/
+	printf("argc:%d\n",argc);/*包括选项的参数字段总个数+1*/
+	if(optind == argc){
+		//printf("0 arg\n");
+		argv[optind] = ".";
+		argc ++;
+	}
 
+		while (optind < argc){
+			
+	
+			path = argv[optind];
+		//	printf("%s:\n",path);
+			ret = stat(path,&st);
+			if (ret == -1){
+              printf("error: %m\n");
+      		}
+			if(S_ISREG(st.st_mode)){
+				//printf("is file\n");
+		
+        		if(((mflg&(st.st_mtime>time_limit))|(!mflg))&
+                (lflg&(st.st_size>low)|(!lflg))&
+                (hflg&(st.st_size<high)|(!hflg))
+                 ){
+					strftime(format_time, BUFLEN, "%F", localtime(&(st.st_mtime)));
+					printf("%s\t\t",format_time);
+					printf("%ld\t\t",st.st_size);
+            		printf("%s\n",  path);
+					//printf("%ld\t\t",st.st_mtime);
+        		}	
+			}
+			else if(S_ISDIR(st.st_mode)){
+				//printf("is dir\n");
+				dir = opendir(path);
+				if (dir == NULL) {
+					printf("Open directory%s: %s (ERROR %s)\n", 
+						argv[optind], strerror(errno), errno);
+				return 1;//errno 
+			}//dirent
+			if (rflg){
+				printf("start recursion\n");
+				recursion_dir(path);
+			}
+			else
+				while ((entry = readdir(dir)) != NULL){
+					/*printf("158\n");test*/
+ 					sprintf(new_path,"%s/%s",path,entry->d_name);
+        			ret = stat(new_path,&st);//lstat stat
+			
+					if (ret == -1){
+               			printf("error: %m\n");
+					}
+					else if(!S_ISDIR(st.st_mode)&((mflg&(st.st_mtime>time_limit))|(!mflg))&
+						(lflg&(st.st_size>low)|(!lflg))&
+						(hflg&(st.st_size<high)|(!hflg))&
+						(!aflg&(entry->d_name[0]!='.')|(aflg))
+						){    		
+						strftime(format_time, BUFLEN, "%F", localtime(&(st.st_mtime)));
+						printf("%s\t\t",format_time);
+						printf("%ld\t\t",st.st_size);
+            			printf("%s\n",  entry->d_name);
+						//printf("%ld\t\t",st.st_mtime);
+					
+					}
+				}
+		
+			closedir(dir);
+	
+			}
+			optind++;
+		}
+	
   if(errflg) 
-  {
-     fprintf(stderr,"Usage:     [OPTION]\n\n",
-        
+  {//stdio.h
+     fprintf(stderr,"Usage:    ./list_v6 [OPTION]\n\n",
     "Mandatory arguments to long options are mandatory for short options too.\n",
     "\t-r, --recursion   show the subfile\n ",
     "\t-a, --all         show the file start with dot\n",
@@ -72,10 +203,9 @@ int main(int argc, char **argv)
 
     "Report bugs to <lyuying@bupt.edu.cn>.\n"
      );
-    exit(0);
+    
   }
 
-  /* 省略部分 */
 
   return 0;
 } 
